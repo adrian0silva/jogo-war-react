46942e1c39b09114b8fd9af2410628bf435391dc
adrian0silva <adrianosilvaparola@gmail.com>
2019-03-20 02:48:01 -0300

    adicionando bootstrap e fontawesome

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.css';
+import './bootstrap.min.css';
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { applyMiddleware,createStore } from 'redux'
